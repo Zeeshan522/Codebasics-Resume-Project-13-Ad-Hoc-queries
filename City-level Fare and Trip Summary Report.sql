eb1be52722b27ab88014a30cbe5bacdc1b6d3272
Zeeshan Ahmed <mdzeeshan522@gmail.com>
2024-12-14 08:35:32 +0530

    Add files via upload

---
+++ City-level Fare and Trip Summary Report.sql
@@ -0,0 +1,23 @@
+# select the database trip_db to run this query
+
+Create index i_city_name on dim_city(city_name);
+create index i_trip_id on fact_trips (trip_id);
+
+with city_level_aggregation as
+(select 
+dc.city_name, 
+count(distinct(ft.trip_id)) as total_trips,
+sum(ft.fare_amount)/sum(ft.distance_travelled_km) as avg_fare_per_km,
+sum(ft.fare_amount)/count(distinct(ft.trip_id)) as avg_fare_per_trip
+from 
+dim_city dc join
+fact_trips ft on ft.city_id = dc.city_id
+group by city_name)
+select  distinct t1.city_name, 
+total_trips, 
+concat("INR ", avg_fare_per_km) as avg_fare_per_km, 
+concat("INR ", avg_fare_per_trip) as avg_fare_per_trip, 
+concat(round((total_trips/count((ft.trip_id)) over ())*100, 2)," %") as percent_contribution_to_total_trips
+from city_level_aggregation t1 join dim_city dc on t1.city_name = dc.city_name
+join fact_trips ft on ft. city_id = dc.city_id
+order by total_trips desc;
